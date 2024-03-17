5a691f419d6de695752002e47427137038bd093e
KirShein71 <103045855+KirShein71@users.noreply.github.com>
2024-03-17 12:35:11 +0300

    fix

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -41,9 +41,9 @@ const Login = observer(() => {
   };
 
   const handleInputClick = () => {
-    if (clicked) {
-      const hint = document.querySelector('.show-hint::before');
-      hint.style.display = 'none';
+    if (!clicked) {
+      setClicked(true);
+      setPhone('8');
     }
   };
 
@@ -70,7 +70,7 @@ const Login = observer(() => {
           <form onSubmit={handleSubmit} ref={form}>
             <div>
               <input
-                className="phone__input show-hint"
+                className="phone__input"
                 name="phone"
                 value={clicked ? phone : ''}
                 onChange={handleInputPhone}
