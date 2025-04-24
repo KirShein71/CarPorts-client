f1af7cbf5b3ad84bb334ca49e4cf447cf848d5c0
KirShein71 <103045855+KirShein71@users.noreply.github.com>
2025-04-24 11:45:54 +0300

    fix

--- src/components/OrderMaterialsList/MaterialProject.jsx
+++ src/components/OrderMaterialsList/MaterialProject.jsx
@@ -17,18 +17,30 @@ function MaterialProject({
   projectNoColorCheckbox,
   user,
 }) {
-  const [sortOrder, setSortOrder] = React.useState('desc');
-  const [sortField, setSortField] = React.useState('prop.name');
+  const [sortOrder, setSortOrder] = React.useState('asc');
+  const [sortField, setSortField] = React.useState('prop.number');
 
   const sortedProps = [...props].sort((a, b) => {
-    if (sortField === 'prop.name') {
-      if (a.name < b.name) {
-        return sortOrder === 'asc' ? -1 : 1;
-      } else if (a.name > b.name) {
-        return sortOrder === 'asc' ? 1 : -1;
-      } else {
-        return 0;
-      }
+    const aValue =
+      sortField === 'prop.number'
+        ? a.number
+        : moment(a.agreement_date, 'YYYY/MM/DD')
+            .businessAdd(a.expiration_date, 'days')
+            .businessAdd(a.design_period, 'days');
+
+    const bValue =
+      sortField === 'prop.number'
+        ? b.number
+        : moment(b.agreement_date, 'YYYY/MM/DD')
+            .businessAdd(b.expiration_date, 'days')
+            .businessAdd(b.design_period, 'days');
+
+    if (aValue < bValue) {
+      return sortOrder === 'asc' ? -1 : 1;
+    } else if (aValue > bValue) {
+      return sortOrder === 'asc' ? 1 : -1;
+    } else {
+      return 0;
     }
   });
 
@@ -51,82 +63,114 @@ function MaterialProject({
             </div>
           </div>
           <div className="table-container">
-            <Table striped bordered size="sm" className="mt-3">
+            <Table striped bordered size="sm" className="projectmaterial-table mt-3">
               <thead>
                 <tr>
-                  <th>Номер проекта</th>
-                  <th className="production_column">
+                  <th style={{ width: '20%' }}>
                     <div style={{ cursor: 'pointer', display: 'flex' }}>
-                      <div>Название проекта</div>{' '}
+                      <div>Номер</div>{' '}
                       <img
-                        onClick={() => handleSort('prop.name')}
+                        onClick={() => handleSort('prop.number')}
                         style={{ marginLeft: '10px', width: '24px', height: '24px' }}
                         src="./img/sort.png"
                         alt="icon_sort"
                       />
                     </div>
                   </th>
-                  <th>Дедлайн производства</th>
-                  <th>Счёт</th>
-                  <th>Дата оплаты</th>
-                  <th>Дата готовности</th>
-                  <th>Даты отгрузки</th>
-                  <th>Цвет</th>
-                  <th></th>
+                  <th className="production_column" style={{ width: '30%' }}>
+                    Название проекта
+                  </th>
+                  <th style={{ textAlign: 'center', width: '25%' }}>
+                    <div style={{ cursor: 'pointer', display: 'flex' }}>
+                      <div>Дедлайн</div>{' '}
+                      <img
+                        onClick={() => handleSort('deadline')}
+                        style={{ marginLeft: '10px', width: '24px', height: '24px' }}
+                        src="./img/sort.png"
+                        alt="icon_sort"
+                      />
+                    </div>
+                  </th>
+                  <th style={{ textAlign: 'center', width: '25%' }}>Счёт</th>
+                  <th style={{ textAlign: 'center', width: '25%' }}>Оплата</th>
+                  <th style={{ textAlign: 'center', width: '25%' }}>Готовность</th>
+                  <th style={{ textAlign: 'center', width: '25%' }}>Отгрузка</th>
+                  <th style={{ textAlign: 'center', width: '25%' }}>Цвет</th>
+                  <th style={{ width: '25%' }}></th>
                 </tr>
               </thead>
               <tbody>
                 {sortedProps
                   .filter((prop) => !projectNoDatePaymentCheckbox || prop.date_payment === null)
                   .filter((prop) => !projectNoColorCheckbox || prop.color === null)
-                  .sort((a, b) => a.id - b.id)
+
                   .map((prop) => (
                     <tr>
                       <td>{prop.number}</td>
                       <td className="production_column">{prop.name}</td>
-                      <td>
+                      <td style={{ textAlign: 'center' }}>
                         {moment(prop.agreement_date, 'YYYY/MM/DD')
                           .businessAdd(prop.expiration_date, 'days')
                           .businessAdd(prop.design_period, 'days')
                           .format('DD.MM.YYYY')}
                       </td>
-                      <td style={{ cursor: 'pointer' }} onClick={() => handleUpdateClick(prop.id)}>
+                      <td
+                        style={{ cursor: 'pointer', textAlign: 'center' }}
+                        onClick={() => handleUpdateClick(prop.id)}>
                         {prop.check ? (
                           <>{prop.check}</>
                         ) : (
-                          <span style={{ color: 'red', fontWeight: 600, cursor: 'pointer' }}>
+                          <span
+                            style={{
+                              color: 'red',
+                              fontWeight: 600,
+                              cursor: 'pointer',
+                              textAlign: 'center',
+                            }}>
                             +
                           </span>
                         )}
                       </td>
                       <td
-                        style={{ cursor: 'pointer' }}
+                        style={{ cursor: 'pointer', textAlign: 'center' }}
                         onClick={
                           user.isManagerProduction ? undefined : () => handlePaymentDate(prop.id)
                         }>
                         {prop.date_payment ? (
                           <Moment format="DD.MM.YYYY">{prop.date_payment}</Moment>
                         ) : (
-                          <span style={{ color: 'red', fontWeight: 600, cursor: 'pointer' }}>
+                          <span
+                            style={{
+                              color: 'red',
+                              fontWeight: 600,
+                              cursor: 'pointer',
+                              textAlign: 'center',
+                            }}>
                             +
                           </span>
                         )}
                       </td>
                       <td
-                        style={{ cursor: 'pointer' }}
+                        style={{ cursor: 'pointer', textAlign: 'center' }}
                         onClick={
                           user.isManagerProduction ? undefined : () => hadleReadyDate(prop.id)
                         }>
                         {prop.ready_date ? (
                           <Moment format="DD.MM.YYYY">{prop.ready_date}</Moment>
                         ) : (
-                          <span style={{ color: 'red', fontWeight: 600, cursor: 'pointer' }}>
+                          <span
+                            style={{
+                              color: 'red',
+                              fontWeight: 600,
+                              cursor: 'pointer',
+                              textAlign: 'center',
+                            }}>
                             +
                           </span>
                         )}
                       </td>
                       <td
-                        style={{ cursor: 'pointer' }}
+                        style={{ cursor: 'pointer', textAlign: 'center' }}
                         onClick={
                           user.isManagerProduction ? undefined : () => hadleShippingDate(prop.id)
                         }>
@@ -136,7 +180,7 @@ function MaterialProject({
                           <span
                             style={{
                               color: 'red',
-
+                              textAlign: 'center',
                               fontWeight: 600,
                               cursor: 'pointer',
                             }}>
@@ -145,19 +189,25 @@ function MaterialProject({
                         )}
                       </td>
                       <td
-                        style={{ cursor: 'pointer' }}
+                        style={{ cursor: 'pointer', textAlign: 'center' }}
                         onClick={
                           user.isManagerProduction ? undefined : () => handleCreateColor(prop.id)
                         }>
                         {prop.color ? (
                           <>{prop.color}</>
                         ) : (
-                          <span style={{ color: 'red', fontWeight: 600, cursor: 'pointer' }}>
+                          <span
+                            style={{
+                              color: 'red',
+                              fontWeight: 600,
+                              cursor: 'pointer',
+                              textAlign: 'center',
+                            }}>
                             +
                           </span>
                         )}
                       </td>
-                      <td>
+                      <td style={{ textAlign: 'center' }}>
                         <Button
                           size="sm"
                           variant="dark"
--- src/components/OrderMaterialsList/ProjectMaterial.jsx
+++ src/components/OrderMaterialsList/ProjectMaterial.jsx
@@ -39,20 +39,28 @@ function ProjectMaterial({
                 onClick={user.isManagerProduction ? undefined : () => handleCreateMaterial(id)}>
                 Добавить материал
               </Button>
+              <div className="projectmaterial__deadline">
+                Сроки :
+                {moment(agreement_date, 'YYYY/MM/DD')
+                  .businessAdd(expiration_date, 'days')
+                  .businessAdd(design_period, 'days')
+                  .format('DD.MM.YYYY')}
+              </div>
             </div>
           </div>
           <div className="table-container">
-            <Table striped bordered size="sm" className="mt-3">
+            <Table striped bordered size="sm" className="projectmaterial-table mt-3">
               <thead>
                 <tr>
-                  <th className="production_column">Тип материала</th>
-                  <th>Дедлайн производства</th>
-                  <th>Счёт</th>
-                  <th>Дата оплаты</th>
-                  <th>Дата готовности</th>
-                  <th>Даты отгрузки</th>
-                  <th>Цвет</th>
-                  <th></th>
+                  <th style={{ width: '30%' }} className="production_column">
+                    Тип материала
+                  </th>
+                  <th style={{ textAlign: 'center', width: '25%' }}>Счёт</th>
+                  <th style={{ textAlign: 'center', width: '25%' }}>Оплата</th>
+                  <th style={{ textAlign: 'center', width: '25%' }}>Готовность</th>
+                  <th style={{ textAlign: 'center', width: '25%' }}>Отгрузка</th>
+                  <th style={{ textAlign: 'center', width: '25%' }}>Цвет</th>
+                  <th style={{ width: '25%' }}></th>
                 </tr>
               </thead>
               <tbody>
@@ -64,49 +72,63 @@ function ProjectMaterial({
                     return (
                       <tr key={prop.id}>
                         <td className="production_column">{prop.materialName}</td>
-                        <td>
-                          {moment(agreement_date, 'YYYY/MM/DD')
-                            .businessAdd(expiration_date, 'days')
-                            .businessAdd(design_period, 'days')
-                            .format('DD.MM.YYYY')}
-                        </td>
                         <td
-                          style={{ cursor: 'pointer' }}
+                          style={{ cursor: 'pointer', textAlign: 'center' }}
                           onClick={
                             user.isManagerProduction ? undefined : () => handleUpdateClick(prop.id)
                           }>
                           {prop.check ? (
                             <>{prop.check}</>
                           ) : (
-                            <span style={{ color: 'red', fontWeight: 600, cursor: 'pointer' }}>
+                            <span
+                              style={{
+                                color: 'red',
+                                fontWeight: 600,
+                                cursor: 'pointer',
+                                textAlign: 'center',
+                              }}>
                               +
                             </span>
                           )}
                         </td>
                         <td
-                          style={{ cursor: 'pointer' }}
+                          style={{ cursor: 'pointer', textAlign: 'center' }}
                           onClick={
                             user.isManagerProduction ? undefined : () => handlePaymentDate(prop.id)
                           }>
                           {prop.date_payment ? (
                             <Moment format="DD.MM.YYYY">{prop.date_payment}</Moment>
                           ) : (
-                            <span style={{ color: 'red', fontWeight: 600, cursor: 'pointer' }}>
-                              +
-                            </span>
-                          )}
-                        </td>
-                        <td style={{ cursor: 'pointer' }} onClick={() => hadleReadyDate(prop.id)}>
-                          {prop.ready_date ? (
-                            <Moment format="DD.MM.YYYY">{prop.ready_date}</Moment>
-                          ) : (
-                            <span style={{ color: 'red', fontWeight: 600, cursor: 'pointer' }}>
+                            <span
+                              style={{
+                                color: 'red',
+                                fontWeight: 600,
+                                cursor: 'pointer',
+                                textAlign: 'center',
+                              }}>
                               +
                             </span>
                           )}
                         </td>
                         <td
-                          style={{ cursor: 'pointer' }}
+                          style={{ cursor: 'pointer', textAlign: 'center' }}
+                          onClick={() => hadleReadyDate(prop.id)}>
+                          {prop.ready_date ? (
+                            <Moment format="DD.MM.YYYY">{prop.ready_date}</Moment>
+                          ) : (
+                            <span
+                              style={{
+                                color: 'red',
+                                fontWeight: 600,
+                                cursor: 'pointer',
+                                textAlign: 'center',
+                              }}>
+                              +
+                            </span>
+                          )}
+                        </td>
+                        <td
+                          style={{ cursor: 'pointer', textAlign: 'center' }}
                           onClick={
                             user.isManagerProduction ? undefined : () => hadleShippingDate(prop.id)
                           }>
@@ -116,7 +138,7 @@ function ProjectMaterial({
                             <span
                               style={{
                                 color: 'red',
-
+                                textAlign: 'center',
                                 fontWeight: 600,
                                 cursor: 'pointer',
                               }}>
@@ -125,19 +147,25 @@ function ProjectMaterial({
                           )}
                         </td>
                         <td
-                          style={{ cursor: 'pointer' }}
+                          style={{ cursor: 'pointer', textAlign: 'center' }}
                           onClick={
                             user.isManagerProduction ? undefined : () => handleCreateColor(prop.id)
                           }>
                           {prop.color ? (
                             <>{prop.color}</>
                           ) : (
-                            <span style={{ color: 'red', fontWeight: 600, cursor: 'pointer' }}>
+                            <span
+                              style={{
+                                color: 'red',
+                                fontWeight: 600,
+                                cursor: 'pointer',
+                                textAlign: 'center',
+                              }}>
                               +
                             </span>
                           )}
                         </td>
-                        <td>
+                        <td style={{ textAlign: 'center' }}>
                           <Button
                             size="sm"
                             variant="dark"
